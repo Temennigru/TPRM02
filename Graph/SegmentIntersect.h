b570ab8d16f5ab0465d22fd652f3d5801416c5f0
viki <handre@outlook.com>
2014-10-14 19:09:19 -0500

    Created main base for explorer, and cleaned up project. Goes into infinite loop when exploring.

--- Graph/SegmentIntersect.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SEGMENT_INTERSECT_H
-#define _SEGMENT_INTERSECT_H
-
-// Returns true if two edges p1-q1 and p2-q2 cross each other
-template <typename T> extern bool doIntersect(T p1x, T p1y, T q1x, T q1y, T p2x, T p2y, T q2x, T q2y, bool allowCollinearity = false);
-
-#endif
