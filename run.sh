0b3a683ee32fba033af5c9feb0c62366c423f1cf
viki <handre@outlook.com>
2014-10-26 09:54:05 -0500

    Fixed make run

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+. /opt/ros/indigo/setup.bash
+. $HOME/catkin_ws/devel/setup.bash
+sleep 4	
 echo "Setting up ROS"
 cd $HOME/catkin_ws/; \
 echo "Running roscore"; roscore & \
 sleep 2; echo "Running stageros"; rosrun stage_ros stageros src/tp2/world/omniworld_3.world & \
 sleep 4; echo "Running controler"; rosrun tp2 explore 2 2 25 25 0.25
-pkill roscore
-pkill stage_ros
